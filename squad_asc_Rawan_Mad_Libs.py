f2ae53ddabaa6b1bbaa7f1367f689799087cfd46
RawanEssam0 <rawanessam343@gmail.com>
2022-09-18 22:01:49 +0200

    Python part1 task

---
+++ squad_asc_Rawan_Mad_Libs.py
@@ -0,0 +1,8 @@
+adj = input("Enter an adjective: ")
+Color = input("Enter a color: ")
+celeb = input("Enter a celebrity name: ")
+food = input("Enter a food: ")
+print("Today is a "+ adj+" day")
+print("The sky is "+Color)
+print(celeb+" is my role model")
+print("I would eat "+ food +" all day") 
